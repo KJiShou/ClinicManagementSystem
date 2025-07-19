48a000eec2b057f5b0f57e8a353b1216d11a7dd9
KJiShou <144637328+KJiShou@users.noreply.github.com>
2025-07-19 18:21:19 +0800

    feat: add file structure

--- src/control/Consultation.java
+++ src/control/Consultation.java
@@ -1,4 +1,11 @@
 package control;
 
 public class Consultation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
 }
