3a77dbcc6e0efa665a4941458d47503f3df092ba
KJiShou <144637328+KJiShou@users.noreply.github.com>
2025-07-26 23:50:10 +0800

    fix(hashedDictionary): complete all function

--- src/adt/HashedDictionary.java
+++ src/adt/HashedDictionary.java
@@ -2,7 +2,6 @@ package adt;
 
 import java.io.Serializable;
 
-// test
 public class HashedDictionary<K, V> implements DictionaryInterface<K, V>, Serializable {
     private Node<K, V>[] hashedDictionary;
     private int numberOfEntries;
@@ -20,55 +19,24 @@ public class HashedDictionary<K, V> implements DictionaryInterface<K, V>, Serial
         numberOfEntries = 0;
     }
 
-    // Private inner class Node as the data types for the hashed dictionary
-    private class Node<K, V> implements java.io.Serializable {
-        private K key;
-        private V value;
-        private Node<K, V> nextNode;
+    // get Node data in string
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < hashedDictionary.length; i++) {
+            if (hashedDictionary[i] == null) {
+                output.append("No Data\n");
+            } else {
+                Node<K, V> currentNode = hashedDictionary[i];
+                while (currentNode != null) {
+                    output.append(currentNode.key).append(currentNode.value).append(",");
+                    currentNode = currentNode.nextNode;
+                }
 
-        // constructor without nextNode
-        private Node(K searchKey, V searchValue) {
-            this.key = searchKey;
-            this.value = searchValue;
-            this.nextNode = null;
+                output.append("\n");
+            }
+            output.append("\n");
         }
-
-        // constructor with nextNode
-        private Node(K searchKey, V searchValue, Node<K, V> nextNode) {
-            this.key = searchKey;
-            this.value = searchValue;
-            this.nextNode = nextNode;
-        }
-
-        // setter for Node key
-        private void setKey(K key) {
-            this.key = key;
-        }
-
-        // setter for Node value
-        private void setValue(V value) {
-            this.value = value;
-        }
-
-        // setter for next Node
-        private void setNextNode(Node<K, V> nextNode) {
-            this.nextNode = nextNode;
-        }
-
-        // getter for Node key
-        private K getKey() {
-            return key;
-        }
-
-        // getter for Node value
-        private V getValue() {
-            return value;
-        }
-
-        // getter for next Node
-        private Node<K, V> getNextNode() {
-            return nextNode;
-        }
+        return output.toString();
     }
 
     @Override
@@ -82,52 +50,76 @@ public class HashedDictionary<K, V> implements DictionaryInterface<K, V>, Serial
         int index = getHashIndex(key);
 
         if (hashedDictionary[index] == null) {
+            // no node in this index
+            hashedDictionary[index] = new Node<K, V>(key, value);
+            numberOfEntries++;
+        } else {
+            Node<K, V> currentNode = hashedDictionary[index];
+            Node<K, V> nodeBefore = null;
 
+            while (currentNode != null && !key.equals(currentNode.key)) {
+                nodeBefore = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+
+            if (currentNode == null) {
+                Node<K, V> newNode = new Node<K, V>(key, value);
+                nodeBefore.nextNode = newNode;
+                numberOfEntries++;
+            }else {
+                oldValue = currentNode.value;
+                currentNode.value = value;
+            }
         }
-        return null;
+        return oldValue;
     }
-
 
     @Override
     public V remove(K key) {
-        return null;
+        V removedValue = null;
+
+        int index = getHashIndex(key);
+
+        // search chain beginning at hashedDictionary[index] for a node that contains key
+        Node<K, V> nodeBefore = null;
+        Node<K, V> currentNode = hashedDictionary[index];
+
+        while ((currentNode != null) && !key.equals(currentNode.key)) {
+            nodeBefore = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+
+        if (currentNode != null) {
+            // key found, get value for return and then remove node
+            removedValue = currentNode.value;
+
+            if (nodeBefore == null) {
+                hashedDictionary[index] = currentNode.nextNode;
+            } else {
+                nodeBefore.nextNode = currentNode.nextNode;
+            }
+
+            numberOfEntries--;
+        }
+        return removedValue;
     }
 
     @Override
     public V getValue(K key) {
-        return null;
-    }
+        V result = null;
+        int index = getHashIndex(key);
 
-    @Override
-    public boolean contains(K key) {
-        return false;
-    }
+        Node<K, V> currentNode = hashedDictionary[index];
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean isFull() {
-        return false;
-    }
-
-    @Override
-    public int getSize() {
-        return 0;
-    }
-
-    @Override
-    public void clear() {
-    }
-
-    private int getHashIndex(K key) {
-        int hashIndex = key.hashCode() % hashedDictionary.length;
-        if (hashIndex < 0) {
-            hashIndex = hashIndex + hashedDictionary.length;
+        while ((currentNode != null) && !key.equals(currentNode.key)) {
+            currentNode = currentNode.nextNode;
         }
-        return hashIndex;
+
+        if (currentNode != null) {
+            result = currentNode.value;
+        }
+
+        return result;
     }
 
     private void rehash() {
@@ -142,9 +134,16 @@ public class HashedDictionary<K, V> implements DictionaryInterface<K, V>, Serial
             Node<K, V> currentNode = oldHashedDictionary[i];
 
             while (currentNode != null) {
-                add(currentNode.getKey(), currentNode.getValue());
-                currentNode = currentNode.getNextNode();
+                add(currentNode.key, currentNode.value);
+                currentNode = currentNode.nextNode;
             }
+        }
+    }
+
+    @Override
+    public void clear() {
+        for (int index = 0; index < hashedDictionary.length; index++) {
+            hashedDictionary[index] = null;
         }
     }
 
@@ -153,6 +152,37 @@ public class HashedDictionary<K, V> implements DictionaryInterface<K, V>, Serial
      */
     private boolean isHashedDictionaryTooFull() {
         return numberOfEntries > MAX_LOAD_FACTOR * hashedDictionary.length;
+    }
+
+    // the following methods that appear between the dashed lines
+    // are the same as for open addressing
+    // ----------------------------------------------------------
+    @Override
+    public boolean contains(K key) {
+        return getValue(key) != null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numberOfEntries == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return numberOfEntries;
+    }
+
+    private int getHashIndex(K key) {
+        int hashIndex = key.hashCode() % hashedDictionary.length;
+        if (hashIndex < 0) {
+            hashIndex = hashIndex + hashedDictionary.length;
+        }
+        return hashIndex;
     }
 
     // get the next prime number
@@ -177,24 +207,26 @@ public class HashedDictionary<K, V> implements DictionaryInterface<K, V>, Serial
         }
         return true;
     }
+    // ----------------------------------------------------------
 
-    // get Node data in string
-    public String toString() {
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < hashedDictionary.length; i++) {
-            if (hashedDictionary[i] == null) {
-                output.append("No Data\n");
-            } else {
-                Node<K, V> currentNode = hashedDictionary[i];
-                while (currentNode != null) {
-                    output.append(currentNode.getKey()).append(currentNode.getValue()).append(",");
-                    currentNode = currentNode.getNextNode();
-                }
+    // Private inner class Node as the data types for the hashed dictionary
+    private class Node<K, V> implements java.io.Serializable {
+        private K key;
+        private V value;
+        private Node<K, V> nextNode;
 
-                output.append("\n");
-            }
-            output.append("\n");
+        // constructor without nextNode
+        private Node(K searchKey, V searchValue) {
+            this.key = searchKey;
+            this.value = searchValue;
+            this.nextNode = null;
         }
-        return output.toString();
+
+        // constructor with nextNode
+        private Node(K searchKey, V searchValue, Node<K, V> nextNode) {
+            this.key = searchKey;
+            this.value = searchValue;
+            this.nextNode = nextNode;
+        }
     }
 }
