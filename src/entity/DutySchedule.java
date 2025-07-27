14a8c321b63b1465923d2d0cbc8a66e00247d35c
HongJunChea <cheahongjun@gmail.com>
2025-07-27 09:20:58 +0800

    feat():ADT and class

---
+++ src/entity/DutySchedule.java
@@ -0,0 +1,65 @@
+package entity;
+
+import java.time.LocalTime;
+
+public class DutySchedule {
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int day;
+    private int month;
+    private int year;
+
+    public DutySchedule(LocalTime startTime, LocalTime endTime, int day, int month, int year) {
+        this.startTime = LocalTime.now();
+        this.endTime = LocalTime.now();
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return day + " " + startTime + " - " + endTime;
+    }
+
+}
