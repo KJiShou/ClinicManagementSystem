36bf62b15f9bcdbe0f1ae7379736e8055a09515e
JiShou <jihsoukong@gmail.com>
2025-08-06 22:35:55 +0800

    feat(UI): Added MessageUI utility to print selection UI

--- src/boundary/PharmacyUI.java
+++ src/boundary/PharmacyUI.java
@@ -1,4 +1,27 @@
 package boundary;
 
+import adt.LinkedQueue;
+import adt.QueueInterface;
+import utility.MessageUI;
+
+import java.util.Scanner;
+
 public class PharmacyUI {
+    QueueInterface<String> choiceQueue;
+    MessageUI UI;
+
+    public PharmacyUI() {
+        choiceQueue = new LinkedQueue<String>();
+        UI = new MessageUI();
+    }
+
+    public Integer mainMenu() {
+        // choice 1
+        choiceQueue.enqueue("Check Sales Item");
+        choiceQueue.enqueue("Stock In Sales Item");
+        choiceQueue.enqueue("Update Sales Item details");
+        choiceQueue.enqueue("Stock Out Sales Item");
+
+        return UI.mainUI("Welcome to Pharmacy Management System", choiceQueue);
+    }
 }
--- src/utility/MessageUI.java
+++ src/utility/MessageUI.java
@@ -1,4 +1,52 @@
 package utility;
 
+import adt.LinkedQueue;
+import adt.QueueInterface;
+
+import java.util.Scanner;
+
 public class MessageUI {
+    Scanner scanner;
+    Integer choice;
+    public MessageUI() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void getChoice(Integer maxChoice) {
+        while (true) {
+            System.out.print("Enter your choice > ");
+            choice = scanner.nextInt();
+            if (choice == 999) return;
+            if (choice <=0 || choice > maxChoice) {
+                System.out.println("Invalid choice. Pls Enter again.");
+            } else {
+                return;
+            }
+        }
+    }
+
+    public static String center(String text, int width) {
+        if (text == null || width <= text.length()) {
+            return text;
+        }
+        int padding = (width - text.length()) / 2;
+        return " ".repeat(padding) +
+                text;
+    }
+
+    public Integer mainUI(String title, QueueInterface<String> choiceQueue) {
+        int size = choiceQueue.size();
+        System.out.println("========================================================");
+        System.out.printf("|| %-50s ||\n", center(title, 50));
+        System.out.println("========================================================");
+        int count = 0;
+         while (!choiceQueue.isEmpty()) {
+            System.out.printf("|| [ %d ] %-44s ||\n", ++count, choiceQueue.dequeue());
+            System.out.println("========================================================");
+        }
+        System.out.println("|| [999] Exit                                         ||");
+        System.out.println("========================================================");
+        getChoice(size);
+        return choice;
+    }
 }
