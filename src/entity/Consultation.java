89e9c52dfb78a9dc2efc6d238c2a90c7e9b18273
Mingyverse <teoh.yongming456@gmail.com>
2025-08-15 23:35:16 +0800

    fix(ConsultationEntity): change Status as enum

--- src/entity/Consultation.java
+++ src/entity/Consultation.java
@@ -9,7 +9,7 @@ public class Consultation {
     private Patient patient;
     private Doctor doctor;
     private LocalDate consultatonDate;
-    public String status;
+    public Status status;
     private String notes;
     private LocalTime startTime;
     private LocalTime endTime;
@@ -17,7 +17,7 @@ public class Consultation {
 
     public Consultation() {}
 
-    public Consultation(UUID id, Patient patient, Doctor doctor, LocalDate consultatonDate, String status, String notes, LocalTime startTime, LocalTime endTime, float totalPayment) {
+    public Consultation(UUID id, Patient patient, Doctor doctor, LocalDate consultatonDate, Status status, String notes, LocalTime startTime, LocalTime endTime, float totalPayment) {
         this.id = id;
         this.patient = patient;
         this.doctor = doctor;
@@ -36,7 +36,7 @@ public class Consultation {
     public Doctor getDoctor() { return doctor; }
     public UUID getDoctorId() {return doctor.getUserID(); }
     public LocalDate getConsultatonDate() { return consultatonDate; }
-    public String getStatus() { return status; }
+    public Status getStatus() { return status; }
     public String getNotes() { return notes; }
     public LocalTime getStartTime() { return startTime; }
     public LocalTime getEndTime() { return endTime; }
@@ -47,7 +47,7 @@ public class Consultation {
     public void setPatient(Patient patient) { this.patient = patient; }
     public void setDoctor(Doctor doctor) { this.doctor = doctor; }
     public void setConsultatonDate(LocalDate consultatonDate) { this.consultatonDate = consultatonDate; }
-    public void setStatus(String status) { this.status = status; }
+    public void setStatus(Status status) { this.status = status; }
     public void setNotes(String notes) { this.notes = notes; }
     public void setStartTime(LocalTime startTime) { this.startTime = startTime; }
     public void setEndTime(LocalTime endTime) { this.endTime = endTime; }
@@ -63,5 +63,12 @@ public class Consultation {
                 "\nStart Time      : " + startTime +
                 "\nEnd Time        : " + endTime +
                 "\nTotal Payment   : " + totalPayment;
+    }
+
+    private enum Status {
+        WAITING,
+        BILLING,
+        IN_PROGRESS,
+        COMPLETED
     }
 }
