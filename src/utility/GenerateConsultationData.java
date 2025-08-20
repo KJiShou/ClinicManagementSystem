0917dc5a7f242d85ff4f03d10db66e86f83330df
Mingyverse <teoh.yongming456@gmail.com>
2025-08-20 15:06:45 +0800

    feat(ConsultationData): one data generation sample

---
+++ src/utility/GenerateConsultationData.java
@@ -0,0 +1,31 @@
+package utility;
+
+import adt.ArrayList;
+import adt.ListInterface;
+import entity.Doctor;
+import entity.Patient;
+import entity.User;
+import java.text.ParseException;
+import java.util.UUID;
+
+public class GenerateConsultationData {
+
+    public static ListInterface<Consultation> createSampleConsultation() throws ParseException {
+        ListInterface<Consultation> consultations = new ArrayList<>();
+
+        consultations.add(new Consultation(
+                UUID.randomUUID(),
+                null, //patient
+                null, //doctor
+                "2025-05-19",
+                Consultation.Status.COMPLETED,
+                "Fever, cough",
+                "10:00",
+                "11:30",
+                200.00
+        ));
+
+        return consultations;
+    }
+
+}
